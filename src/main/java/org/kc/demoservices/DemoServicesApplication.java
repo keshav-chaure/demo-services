291c99602db5c6be9deaa28414e2ff596a7dfa51
keshav-chaure <keshav.chaure@gmail.com>
2019-04-24 16:01:44 +0530

    first commit

---
+++ src/main/java/org/kc/demoservices/DemoServicesApplication.java
@@ -0,0 +1,13 @@
+package org.kc.demoservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServicesApplication.class, args);
+	}
+
+}
